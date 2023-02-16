98f13fe77225575e25dc451464281114b502f6b1
Eugênio Francisco da Silva <eugeniofr17@gmail.com>
2023-02-16 10:14:56 -0300

    Criando o projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import { Title, Container } from './style'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Container>
+        <Title>Minha agenda</Title>
+        
+      </Container>
       
     </div>
   );
